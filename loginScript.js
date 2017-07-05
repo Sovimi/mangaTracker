fbeec2fc6efc45edc84e57c43d3db6d7dc93ddaf
Sofia Militão <sofia_vieira@live.com.pt>
2017-07-05 14:01:18 +0100

    added a data file

--- loginScript.js
+++ loginScript.js
@@ -1,20 +1,60 @@
-var user = {
-    email:"admin@demo",
-    password:"admin"
-};
+var userData;
 
-function myFunction() {
-    var x,y, text;
+function authenticator() {
+    var x,y,text;
 
-    // Get the value of the input field with id="numb"
     x = document.getElementById("inputEmail").value;
     y = document.getElementById("inputPassword").value;
 
-    // If x is Not a Number or less than one or greater than 10
-    if (x.isEqualNode(user.email) && y.isEqualNode(user.password)) {
-        text = "Input vvalid";
-    } else {
-        text = "Not a user";
+
+    for (var i = userData.length - 1; i >= 0; i--) {
+
+        if (x === userData.users[i].email && y === userData.users[i].password) {
+            text = "valid user";
+        } else {
+            text = "Not a user";
+        }
     }
-    document.getElementById("demo").innerHTML = text;
+    document.getElementById("demoTT").innerHTML = text;
+}
+
+function saveUser(data){
+    var x,y,myJSON;
+    userData = JSON.parse(data);  
+
+    x = document.getElementById("inputEmail").value;
+    y = document.getElementById("inputPassword").value;
+    z = document.getElementById("inputUsername").value;
+
+    userData.users.push({
+        email: x,
+        password: y,
+        username: z
+    });
+
+    myJSON = JSON.stringify(userData);
+    //localStorage.setItem("data", myJSON);
+}
+
+
+function loadDoc(url, cFunction) {
+  var xhttp;
+
+    document.getElementById("DemoTT").innerHTML = "TESTING DAMMIT";
+  xhttp=new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      cFunction(this);
+    }
+  };
+  //True for asynchronus mode
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function init(xhttp) {
+    //DO ALL THE STUFF WITH THE DATA HERE
+    this.userData = JSON.parse(xhttp.responseText);
+    //document.getElementById("DemoTT").innerHTML = xhttp.responseText;
 }
