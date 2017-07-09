c9eda5a2aab0ab9037409a9454d98c6d18122f97
Sofia Militão <sofia_vieira@live.com.pt>
2017-07-09 17:37:00 +0100

    changes to search, incomplete manga information pages, changes in scripts

--- loginScript.js
+++ loginScript.js
@@ -14,6 +14,7 @@ function authenticator() {
 
         if (x === userData.users[i].email && y === userData.users[i].password) {
             text = "valid user";
+             window.location = 'home.html';
         } else {
             text = "Not a user";
         }
@@ -84,63 +85,9 @@ function loadJSON(jsonFile) {
         return false;
     }
         
-    http_request.open("GET", jsonFile, true);
+    http_request.open("GET", jsonFile, false);
         
     http_request.send("");
     //return http_request.responseXML;
     return http_request.responseText;
-}
-
-function loadXML(xmlFile) {
-    http_request = false;
-        
-        if (window.XMLHttpRequest) { // Mozilla, Safari,...
-            http_request = new window.XMLHttpRequest();         
-            
-        } else if (window.ActiveXObject) { // IE
-            try {
-                http_request = new ActiveXObject("Microsoft.XMLHTTP");
-            } catch (e) {
-                try {
-                    http_request = new ActiveXObject("Microsoft.XMLHTTP");
-                } catch (e) {}
-            }
-        }
-
-        if (!http_request) {
-            alert('Giving up :( Cannot create an XMLHTTP instance');
-            return false;
-        }
-        
-        http_request.open("GET",xmlFile,false);
-        
-        http_request.send("");
-        return http_request.responseXML;
-}
-
-function getSearchResults() {
-  var x, i, xmlDoc, table;
-  xmlDoc = loadXML("manga.xml");
-  table = "";
-  x = xmlDoc.getElementsByTagName("MANGA");
-  for (var j = 0; j<x.length; j++) {
-      table += "<div class='row'>";
-       for (i = 0; i < 6 || i<x.length; i++, j++) { 
-        table += "<div class='col-sm-2'><div class='cover-card'></div><a href='" + 
-        x[i].getElementsByTagName("PAGE-URL")[0].childNodes[0].childNodes[0].nodeValue +
-        "'<img src='" + 
-        x[i].getElementsByTagName("IMG-URL")[0].childNodes[0].childNodes[0].nodeValue +
-        "' alt=''>></a><div class='cover-data'><a href='"+
-        x[i].getElementsByTagName("PAGE-URL")[0].childNodes[0].childNodes[0].nodeValue +
-        "'>"+
-        x[i].getElementsByTagName("TITLE")[0].childNodes[0].childNodes[0].nodeValue +
-        "</a></div><span><div title='type' class='manga-info-type'>"+
-        x[i].getElementsByTagName("TYPE")[0].childNodes[0].childNodes[0].nodeValue +
-        "</div><div title='average score' class='manga-info-score'>"+
-        x[i].getElementsByTagName("SCORE")[0].childNodes[0].childNodes[0].nodeValue +
-        "</div></span></div>";
-      }
-  }
-  document.getElementById("search-results-div").innerHTML = table;
-  console.log(table);
 }
--- searchScript.js
+++ searchScript.js
@@ -1,5 +1,6 @@
 var http_request = false;
 var xml_tree;
+var ofyear="", oftype="", ofstatus="", sortby="", input;
 
     function loadXML(xmlFile) {
         
@@ -29,28 +30,61 @@ var xml_tree;
 		return http_request.responseXML;
     }
 
-    function getSearchResults() {
-      var x, i, xmlDoc, table;
-      xmlDoc = loadXML("manga.xml");
-      table = "";
-      x = xmlDoc.getElementsByTagName("MANGA");
-      for (var j = 0; j<x.length; j++) {
-          table += "<div class='row'>";
-           for (i = 0; i < 6 || i<x.length; i++, j++) { 
-            table += "<div class='col-sm-2'><div class='cover-card'></div><a href='" + 
-            x[i].getElementsByTagName("PAGE-URL")[0].childNodes[0].childNodes[0] +
-            "'<img src='" + 
-            x[i].getElementsByTagName("IMG-URL")[0].childNodes[0].childNodes[0] +
-            "' alt=''>></a><div class='cover-data'><a href='"+
-            x[i].getElementsByTagName("PAGE-URL")[0].childNodes[0].childNodes[0]+
-            "'>"+
-            x[i].getElementsByTagName("TITLE")[0].childNodes[0].childNodes[0] +
-            "</a></div><span><div title='type' class='manga-info-type'>"+
-            x[i].getElementsByTagName("TYPE")[0].childNodes[0].childNodes[0] +
-            "</div><div title='average score' class='manga-info-score'>"+
-            x[i].getElementsByTagName("SCORE")[0].childNodes[0].childNodes[0]+
-            "</div></span></div>";
-          }
+function getGridResults(x){
+  table="";
+  for (var j = 0; j<x.length; j++) {
+    table += "<div class='row'>";
+    for (i = 0; i < 6 && i<x.length; i++, j++) { 
+      titleXML = x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue;
+      yearXML = x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue;
+      statusXML = x[i].getElementsByTagName("STATUS")[0].childNodes[0].nodeValue;
+      typeXML = x[i].getElementsByTagName("TYPE")[0].childNodes[0].nodeValue;
+
+      if ((titleXML.toUpperCase() == input.toUpperCase() || 
+          titleXML.toUpperCase().indexOf(input.toUpperCase()) != -1 || 
+          input == null || input == "" || input == "") &&
+          (yearXML == ofyear || ofyear == null || ofyear == "" )&&(
+          statusXML == ofstatus || ofstatus == null || ofstatus == "" )&&(
+          typeXML == oftype || oftype == null || oftype == "")) {
+        table += '<div class="col-sm-2 cover-card"><a id="cover-img" href="' + 
+        x[i].getElementsByTagName('PAGEURL')[0].childNodes[0].nodeValue +
+        '" title="cover card" style="background-image: url(' + 
+        x[i].getElementsByTagName('IMGURL')[0].childNodes[0].nodeValue +
+        ')"></a><div class="cover-data"><a href="'+
+        x[i].getElementsByTagName('PAGEURL')[0].childNodes[0].nodeValue+
+        '">'+
+        titleXML +
+        '</a><span><div title="type" class="manga-info-type">'+
+        typeXML + 
+        '</div><div title="average score" class="manga-info-score">'+
+        x[i].getElementsByTagName('SCORE')[0].childNodes[0].nodeValue+
+        '</div></span></div></div>';
       }
-      document.getElementById("search-results-div").innerHTML = table;
     }
+  }
+  return table;
+}
+
+function getSearchResults(newInput) {
+  var x, i, xmlDoc, table;
+  if (newInput == null) {input="";}
+  else{this.input = newInput;}
+  xmlDoc = loadXML("manga.xml");
+  x = xmlDoc.getElementsByTagName("MANGA");
+
+  document.getElementById("search-results-div").innerHTML = getGridResults(x);
+}
+
+function getSelectedResults(year, type, status, sort){
+  if(year != null) {this.ofyear = year;} else {this.ofyear = "";}
+  if(type != null) {this.oftype = type;} else {this.oftype ="";}
+  if(status != null){ this.ofstatus = status;} else {this.ofstatus="";}
+  if(sort != null) {this.sortby = sort;} else {this.sortby="";}
+  this.getSearchResults('');
+}
+
+function search(ele) {
+  if(event.keyCode == 13) { 
+    getSearchResults(ele.value);  
+  }
+}
